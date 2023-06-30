6d4b3df3b1674bb2b388afa7ffe2c2212dd026c9
nt1dc <nt1dcare@gmail.com>
2023-06-30 11:31:24 +0300

    initial commit

---
+++ src/test/java/com/example/eventconnect/EventConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
